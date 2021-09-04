d9110b7ea7e5ccd1ac39ca22ecd4fb030043be40
Cole Fortner <colefortner@gmail.com>
2021-09-04 19:06:17 -0400

    Add Break and Continue examples

---
+++ src/loops/Break.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class Break {
+    public static void main(String[] args) {
+        int[] someInts = {1, 10, 2, 3, 5, 8, 10};
+        int searchTerm = 10;
+        for (int oneInt : someInts) {
+            if (oneInt == searchTerm) {
+                System.out.println("Found it! " + oneInt);
+//                break;
+            }
+        }
+    }
+}
