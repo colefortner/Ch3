78dbb289c020563e5c5946eba985f42b08e6c392
Cole Fortner <colefortner@gmail.com>
2021-09-06 20:20:36 -0400

    Add ArrayListGradebook

--- src/Gradebooks/ArrayListGradebook.java
+++ src/Gradebooks/ArrayListGradebook.java
@@ -20,5 +20,24 @@ public class ArrayListGradebook {
                 students.add(newStudent);
             }
         } while(!newStudent.equals(""));
+
+        // Get student grades
+        for (String student: students) {
+            System.out.print("Grade for " + student + ": ");
+            Double grade = input.nextDouble();
+            grades.add(grade);
+        }
+
+        // Print class roster
+        System.out.println("\nClass roster:");
+        double sum = 0.0;
+
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i) + " (" + grades.get(i) + ")");
+            sum += grades.get(i);
+        }
+
+        double avg = sum / students.size();
+        System.out.println("Average grade: " + avg);
     }
 }
